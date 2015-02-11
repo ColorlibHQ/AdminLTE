3c0552ab73d2efe43452db7c5a0e57720a36f64c
Abdullah Almsaeed <almasaeed2010@gmail.com>
2015-02-10 20:13:05 -0500

    Removed unused selectors from prepare layout function

--- dist/js/app.js
+++ dist/js/app.js
@@ -46,7 +46,7 @@ $.AdminLTE.options = {
   //Activate sidebar push menu
   sidebarPushMenu: true,
   //Activate sidebar slimscroll if the fixed layout is set (requires SlimScroll Plugin)
-  sidebarSlimScroll: false,
+  sidebarSlimScroll: true,
   //BoxRefresh Plugin
   enableBoxRefresh: true,
   //Bootstrap.js tooltip
@@ -187,16 +187,16 @@ $.AdminLTE.layout = {
     //Get window height and the wrapper height
     var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
     var window_height = $(window).height();
-    var sidebar_height = $(".main-sidebar, .left-side").height();
+    var sidebar_height = $(".sidebar").height();
     //Set the min-height of the content and sidebar based on the
     //the height of the document.
     if ($("body").hasClass("fixed")) {
       $(".content-wrapper, .right-side").css('min-height', window_height - $('.main-footer').outerHeight());
     } else {
       if (window_height >= sidebar_height) {
-        $(".content-wrapper, .main-sidebar, .left-side, .right-side").css('min-height', window_height - neg);
+        $(".content-wrapper, .right-side").css('min-height', window_height - neg);
       } else {
-        $(".content-wrapper, .main-sidebar, .left-side, .right-side").css('min-height', sidebar_height);
+        $(".content-wrapper, .right-side").css('min-height', sidebar_height);
       }
     }
   },
