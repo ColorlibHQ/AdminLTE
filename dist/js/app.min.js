4a4457b5d3ad0ce265342d2e2784dc39110a16cf
Abdullah Almsaeed <almasaeed2010@gmail.com>
2015-02-25 10:01:46 -0500

    Updated version numbers

--- dist/js/app.min.js
+++ dist/js/app.min.js
@@ -1,5 +1,5 @@
-/*! AdminLTE app.js
- * ================
+/*! AdminLTE 2.0.1 app.js
+ * ======================
  * Main JS application file for AdminLTE v2. This file
  * should be included in all pages. It controls some layout
  * options and implements exclusive AdminLTE plugins.
