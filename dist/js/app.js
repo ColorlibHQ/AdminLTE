1b54f8946ddc887bbb7a1b6f0ec3d37b9ef10ac9
Abdullah Almsaeed <almasaeed2010@gmail.com>
2015-03-03 20:04:07 -0500

    Updated versions in JS and CSS files

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,7 @@
  * @Author  Almsaeed Studio
  * @Support <http://www.almsaeedstudio.com>
  * @Email   <support@almsaeedstudio.com>
- * @version 2.0.2
+ * @version 2.0.3
  * @license MIT <http://opensource.org/licenses/MIT>
  */
 
