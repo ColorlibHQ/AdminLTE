d3a547c5d39678599e44de606ca8627a8d337402
Abdullah Almsaeed <almasaeed2010@gmail.com>
2015-08-16 20:50:48 -0400

    Updated version

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,7 @@
  * @Author  Almsaeed Studio
  * @Support <http://www.almsaeedstudio.com>
  * @Email   <support@almsaeedstudio.com>
- * @version 2.2.1
+ * @version 2.3.0
  * @license MIT <http://opensource.org/licenses/MIT>
  */
 
