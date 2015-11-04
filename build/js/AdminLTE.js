17dce579b0bed751a4ac6e563b04a17ec64852ca
Abdullah Almsaeed <almasaeed2010@gmail.com>
2015-11-04 13:51:53 -0500

    Add new js build folder

---
+++ build/js/AdminLTE.js
@@ -0,0 +1,8 @@
+(function ($) {
+  "use strict";
+  class treeview {
+    _do() {
+      alert("Good");
+    }
+  }
+})(jQuery);
---
+++ build/js/treeview.js
@@ -0,0 +1,11 @@
+/**
+ * Treeview.js
+ * Version 1.0
+ * Author  Abdullah Almsaeed
+ * Website Almsaeed Studio <https://almsaeedstudio.com>
+ */
+
+(function($){
+  "use strict";
+
+})(jQuery);
