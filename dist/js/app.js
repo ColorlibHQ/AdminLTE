b064a64d05179ccf7ecda11f7752a280990139c4
limodou <limodou@gmail.com>
2016-01-24 12:02:01 +0800

    add tiles widgets support, this widget comes from Metro-UI-CSS http://metroui.org.ua/. Changes are: 1. add animations.less, colors.less, images.less, reset.less, tiles.less, transform.less, utiles.less, 1-5.jpg 2. change variables.less, add new color names, add @borderRadius used in tiles 3. change AdminLTE.less, remove google font, because it's not locally, add imports for new less files 4. change app.js, add tiles, fitImage widgets and initialize revoke for metro widgets, also add menu item click event process 5. change doc for index.html, index2.html for new tiles entry, and add tiles.html page, but not changed all pages
    Note: carousel of metro is not the same as bootstrap, so the effect is not available

--- dist/js/app.js
+++ dist/js/app.js
@@ -219,6 +219,12 @@ $(function () {
     });
 
   });
+
+  /*
+   * metro init
+   */
+  $.Metro.initWidgets();
+
 });
 
 /* ----------------------------------
@@ -418,12 +424,24 @@ function _init() {
         checkElement.slideDown(animationSpeed, function () {
           //Add the class active to the parent li
           checkElement.addClass('menu-open');
-          parent.find('li.active').removeClass('active');
+          parent.find('li.active.treeview').removeClass('active');
           parent_li.addClass('active');
           //Fix the layout in case the sidebar stretches over the height of the window
           _this.layout.fix();
         });
       }
+      // click some menu item process added by limodou
+      else {
+        if (!$this.is('.active')) {
+          $(menu).find('.active').each(function(index, v){
+            var $item = $(v);
+            if(!$item.is('.treeview'))
+                $item.removeClass('active');
+          });
+          $this.parent('li').addClass('active');
+        }
+      }
+
       //if this isn't a link, prevent the page from being redirected
       if (checkElement.is('.treeview-menu')) {
         e.preventDefault();
@@ -756,3 +774,405 @@ function _init() {
     });
   };
 }(jQuery));
+
+window.isTouchDevice = function() {
+    return (('ontouchstart' in window)
+    || (navigator.MaxTouchPoints > 0)
+    || (navigator.msMaxTouchPoints > 0));
+};
+
+$.widget( "metro.tile" , {
+
+    version: "3.0.0",
+
+    options: {
+        effect: 'slideLeft',
+        period: 4000,
+        duration: 700,
+        easing: 'doubleSqrt',
+        onClick: function(tile){}
+    },
+
+    _frames: {},
+    _currentIndex: 0,
+    _interval: 0,
+    _outPosition: 0,
+    _size: {},
+
+    _create: function () {
+        var that = this, element = this.element, o = this.options;
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = $.parseJSON(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+
+        this._createTransformTile();
+        this._createLiveTile();
+        this._createEvents();
+
+        element.data('tile', this);
+
+    },
+
+    _createEvents: function(){
+        var that = this, element = this.element, o = this.options;
+        var event = isTouchDevice() ? 'touchstart' : 'click';
+
+        element.on(event, function(e){
+            if (element[0].tagName === "A") {
+
+            } else {
+                if (typeof o.onClick === 'function') {
+                    o.onClick(element);
+                } else {
+                    if (typeof window[o.onClick] === 'function') {
+                        window[o.onClick](element);
+                    } else {
+                        var result = eval("(function(){"+o.onClick+"})");
+                        result.call(element);
+                    }
+                }
+            }
+        });
+    },
+
+    _createLiveTile: function(){
+        var that = this, element = this.element, o = this.options;
+        var event_down = isTouchDevice() ? 'touchstart' : 'mouseenter';
+        var event_up = isTouchDevice() ? 'touchend' : 'mouseleave';
+
+        this._frames = element.find(".live-slide");
+        if (this._frames.length <= 1) {return false;}
+
+        $.easing.doubleSqrt = function(t) {return Math.sqrt(Math.sqrt(t));};
+
+        this._size = {
+            'width': element.width(),
+            'height': element.height()
+        };
+
+        element.on(event_down, function(){
+            that.stop();
+        });
+
+        element.on(event_up, function(){
+            that.start();
+        });
+
+        this.start();
+    },
+
+    start: function(){
+        var that = this;
+        this._interval = setInterval(function(){
+            that._animate();
+        }, this.options.period);
+    },
+
+    stop: function(){
+        clearInterval(this._interval);
+    },
+
+    _animate: function(){
+        var currentFrame = this._frames[this._currentIndex], nextFrame;
+        this._currentIndex += 1;
+        if (this._currentIndex >= this._frames.length) {this._currentIndex = 0;}
+        nextFrame = this._frames[this._currentIndex];
+
+        switch (this.options.effect) {
+            case 'slideLeft': this._effectSlideLeft(currentFrame, nextFrame); break;
+            case 'slideRight': this._effectSlideRight(currentFrame, nextFrame); break;
+            case 'slideDown': this._effectSlideDown(currentFrame, nextFrame); break;
+            case 'slideUpDown': this._effectSlideUpDown(currentFrame, nextFrame); break;
+            case 'slideLeftRight': this._effectSlideLeftRight(currentFrame, nextFrame); break;
+            default: this._effectSlideUp(currentFrame, nextFrame);
+        }
+    },
+
+    _effectSlideLeftRight: function(currentFrame, nextFrame){
+        if (this._currentIndex % 2 === 0) {
+            this._effectSlideLeft(currentFrame, nextFrame);
+        } else {
+            this._effectSlideRight(currentFrame, nextFrame);
+        }
+    },
+
+    _effectSlideUpDown: function(currentFrame, nextFrame){
+        if (this._currentIndex % 2 === 0) {
+            this._effectSlideUp(currentFrame, nextFrame);
+        } else {
+            this._effectSlideDown(currentFrame, nextFrame);
+        }
+    },
+
+    _effectSlideUp: function(currentFrame, nextFrame){
+        var _out = this._size.height;
+        var options = {
+            'duration': this.options.duration,
+            'easing': this.options.easing
+        };
+
+        $(currentFrame)
+            .animate({top: -_out}, options);
+        $(nextFrame)
+            .css({top: _out})
+            .show()
+            .animate({top: 0}, options);
+    },
+
+    _effectSlideDown: function(currentFrame, nextFrame){
+        var _out = this._size.height;
+        var options = {
+            'duration': this.options.duration,
+            'easing': this.options.easing
+        };
+
+        $(currentFrame)
+            .animate({top: _out}, options);
+        $(nextFrame)
+            .css({top: -_out})
+            .show()
+            .animate({top: 0}, options);
+    },
+
+    _effectSlideLeft: function(currentFrame, nextFrame){
+        var _out = this._size.width;
+        var options = {
+            'duration': this.options.duration,
+            'easing': this.options.easing
+        };
+
+        $(currentFrame)
+            .animate({left: _out * -1}, options);
+        $(nextFrame)
+            .css({left: _out})
+            .show()
+            .animate({left: 0}, options);
+    },
+
+    _effectSlideRight: function(currentFrame, nextFrame){
+        var _out = this._size.width;
+        var options = {
+            'duration': this.options.duration,
+            'easing': this.options.easing
+        };
+
+        $(currentFrame)
+            .animate({left: _out}, options);
+        $(nextFrame)
+            .css({left: -_out})
+            .show()
+            .animate({left: 0}, options);
+    },
+
+    _createTransformTile: function(){
+        var that = this, element = this.element, o = this.options;
+        var dim = {w: element.width(), h: element.height()};
+        var event_down = isTouchDevice() ? 'touchstart' : 'mousedown';
+        var event_up = isTouchDevice() ? 'touchend' : 'mouseup';
+        var event_leave = isTouchDevice() ? 'touchend' : 'mouseleave';
+
+
+        element.on(event_down, function(e){
+            var X = e.pageX - $(this).offset().left, Y = e.pageY - $(this).offset().top;
+            var transform = 'top';
+
+            if (X < dim.w * 1/3 && (Y < dim.h * 1/2 || Y > dim.h * 1/2 )) {
+                transform = 'left';
+            } else if (X > dim.w * 2/3 && (Y < dim.h * 1/2 || Y > dim.h * 1/2 )) {
+                transform = 'right';
+            } else if (X > dim.w*1/3 && X<dim.w*2/3 && Y > dim.h/2) {
+                transform = 'bottom';
+            }
+
+            $(this).addClass("tile-transform-"+transform);
+
+            //console.log(transform);
+
+            if (element[0].tagName === 'A' && element.attr('href')) {
+                setTimeout(function(){
+                    document.location.href = element.attr('href');
+                }, 500);
+            }
+        });
+
+        element.on(event_up, function(){
+            $(this)
+                .removeClass("tile-transform-left")
+                .removeClass("tile-transform-right")
+                .removeClass("tile-transform-top")
+                .removeClass("tile-transform-bottom");
+        });
+        element.on(event_leave, function(){
+            $(this)
+                .removeClass("tile-transform-left")
+                .removeClass("tile-transform-right")
+                .removeClass("tile-transform-top")
+                .removeClass("tile-transform-bottom");
+        });
+    },
+
+    _destroy: function () {
+    },
+
+    _setOption: function ( key, value ) {
+        this._super('_setOption', key, value);
+    }
+});
+$.widget( "metro.fitImage" , {
+
+    version: "3.0.0",
+
+    options: {
+        shadow: false,
+        overlay: false,
+        type: 'default',
+        frameColor: 'default',
+        format: 'hd' // 'sd'
+    },
+
+    _create: function () {
+        var element = this.element, o = this.options;
+        var parent = element.parent();
+        var i_w, i_h, p_w, p_h;
+        var div, src = element.attr('src');
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = $.parseJSON(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+
+        $("<img/>")
+            .attr('src', src)
+            .load(function(){
+                i_w = this.width;
+                i_h = this.height;
+            }).remove();
+
+        var image_container = $("<div/>").addClass('image-container').css('width', '100%').appendTo(parent);
+        var image_frame = $("<div/>").addClass('frame').appendTo(image_container);
+
+        p_w = image_frame.innerWidth();
+        p_h = image_frame.innerHeight();
+
+        switch (o.format) {
+            case 'sd': p_h = 3 * p_w / 4; break;
+            case 'square': p_h = p_w; break;
+            case 'cycle': p_h = p_w; break;
+            case 'fill-h': p_h = "100%"; image_container.css('height', '100%'); break;
+            case 'fill': p_h = "100%"; image_container.css('height', '100%'); break;
+            default: p_h = 9 * p_w / 16;
+        }
+
+        div = $("<div/>").css({
+            'width': '100%',
+            'height': p_h,
+            'background-image': 'url('+src+')',
+            'background-size': 'cover',
+            'background-repeat': 'no-repeat',
+            'border-radius': o.format === 'cycle' ? '50%' : '0'
+        });
+
+        $(window).on('resize', function(){
+            var p_w = image_frame.innerWidth();
+            var p_h = image_frame.innerHeight();
+
+            switch (o.format) {
+                case 'sd': p_h = 3 * p_w / 4; break;
+                case 'square': p_h = p_w; break;
+                case 'cycle': p_h = p_w; break;
+                case 'fill-h': p_h = "100%"; image_container.css('height', '100%'); break;
+                case 'fill': p_h = "100%"; image_container.css('height', '100%'); break;
+                default: p_h = 9 * p_w / 16;
+            }
+
+            div.css({
+                'height': p_h
+            });
+        });
+
+        if (o.frameColor !== 'default') {
+            if (o.frameColor.isUrl()) {
+                image_frame.css('background-color', o.frameColor);
+            } else {
+                image_frame.addClass(o.frameColor);
+            }
+        }
+        if (o.overlay !== false) {
+            var overlay = $("<div/>").addClass('image-overlay').html(o.overlay).appendTo(image_container);
+        }
+        if (o.shadow !== false) {
+            image_container.addClass('block-shadow');
+        }
+        div.appendTo(image_frame);
+
+        switch (o.type) {
+            case 'diamond': {
+                image_container.addClass('diamond'); div.addClass('image-replacer'); break;
+            }
+            case 'bordered': {
+                image_container.addClass('bordered'); break;
+            }
+            case 'polaroid': {
+                image_container.addClass('polaroid'); break;
+            }
+            case 'handing': {
+                image_container.addClass('handing'); break;
+            }
+            case 'handing-ani': {
+                image_container.addClass('handing ani'); break;
+            }
+            case 'handing-ani-hover': {
+                image_container.addClass('handing ani-hover'); break;
+            }
+        }
+
+        image_container.addClass('image-format-'+ o.format);
+        //element.css('display', 'none');
+        element.remove();
+
+    },
+
+    _destroy: function () {
+    },
+
+    _setOption: function ( key, value ) {
+        this._super('_setOption', key, value);
+    }
+});
+
+$.Metro = {};
+
+$.Metro.initWidgets = function(){
+    var widgets = $("[data-role]");
+
+    $.each(widgets, function(){
+        var $this = $(this), w = this;
+        var roles = $this.data('role').split(/\s*,\s*/);
+        roles.map(function(func){
+            try {
+                //$(w)[func]();
+                if ($.fn[func] !== undefined && $this.data(func+'-initiated') !== true) {
+                    $.fn[func].call($this);
+                    $this.data(func+'-initiated', true);
+                }
+            } catch(e) {
+                if (window.METRO_DEBUG) {
+                    console.log(e.message, e.stack);
+                }
+            }
+        });
+    });
+};
+
