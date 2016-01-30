67934591fecf90ed08afef78518e518867fdff49
Mcat12 <newtoncat12@yahoo.com>
2016-01-30 13:05:28 -0500

    Dynamic versions in footer - Shows Pi-hole and Web Interface versions

--- index.php
+++ index.php
@@ -187,7 +187,8 @@
             <!-- /.content-wrapper -->
             <footer class="main-footer">
                 <div class="pull-right hidden-xs">
-                    <b>Pi-hole Version</b> 2.4
+                    <b>Pi-hole Version </b> <?php echo exec("cd /etc/.pihole/ && git describe --tags --abbrev=0"); ?>
+                    <b>Web Interface Version </b> <?php echo exec("cd /var/www/html/admin/ && git describe --tags --abbrev=0"); ?>
                 </div>
                 <i class="fa fa-github"></i> <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY">Donate</a></strong> if you found this useful.
             </footer>
