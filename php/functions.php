a7f352674e76c484e5e88cf393d32cd1d178e6ed
Mcat12 <newtoncat12@yahoo.com>
2016-02-28 15:36:16 -0500

    Checks password file to check inputted password

--- php/functions.php
+++ php/functions.php
@@ -1,5 +1,5 @@
 <?php
 function checkPass($pass) {
     // Check password
-    return true;
+    return $pass == str_replace(array("\r", "\n"), '', file_get_contents("/etc/pihole/password.txt"));
 }
