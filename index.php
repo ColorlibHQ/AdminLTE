55cc148e8e4e29a12e24d29a74d443f8d94198fd
Verpz <lachlan186@gmail.com>
2016-02-28 11:31:11 +1100

    CSS Fix

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 ?>
 <!-- JS Warn -->
 <div>
-  <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/Verpz/Javascript-Disabled-Warning/53f927723dfb6ec0c9365e299dc2ed418f5c5158/js-warn.css">
-  <input type="checkbox" id="hide" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/Verpz/Javascript-Disabled-Warning-Popup/b10b9ebe28a92ffe746eb7e6fb6e0d3497c3afdd/js-warn.css">
+  <input type="checkbox" id="js-hide" />
   <div class="js-warn" id="js-warn-exit"><h1>Javascript Is Disabled</h1><p>Javascript seems to be disabled. This will break some site features.</p>
-  <p>To enable Javascript click <a href="http://www.enable-javascript.com/" target="_blank">here</a></p><label for="hide">Close</label></div>
+  <p>To enable Javascript click <a href="http://www.enable-javascript.com/" target="_blank">here</a></p><label for="js-hide">Close</label></div>
   <script>var jswarn = document.getElementById("js-warn-exit"); jswarn.parentNode.removeChild(jswarn);</script>
 </div>
 <!-- / JS Warn -->
