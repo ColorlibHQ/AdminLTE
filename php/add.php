05e7ebe7dd2216d4f240cd04d985b420e079b3af
Mcat12 <newtoncat12@yahoo.com>
2016-05-06 16:14:32 -0400

    Implement CORS
    Only allows requests from http://pi.hole and http://<Pi's IP>

--- php/add.php
+++ php/add.php
@@ -1,12 +1,18 @@
 <?php
 if(!isset($_POST['domain'], $_POST['list'], $_POST['token']))
-    die();
+    die("Missing POST variables");
+
+// Check CORS
+if($_SERVER['HTTP_ORIGIN'] != "http://pi.hole" && $_SERVER['HTTP_ORIGIN'] != "http://${_SERVER['SERVER_ADDR']}")
+    die("Failed CORS");
+
+header("Access-Control-Allow-Origin: ${_SERVER['HTTP_ORIGIN']}");
 
 session_start();
 
 // Check CSRF token
 if(!hash_equals($_SESSION['token'], $_POST['token']))
-    die("Wrong token!");
+    die("Wrong token");
 
 switch($_POST['list']) {
     case "white":
