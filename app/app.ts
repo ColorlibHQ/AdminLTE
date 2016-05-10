503b9051344081965a5b6291003cd35386137ca3
Marvin Perez <marvin.perez@coherentasia.com>
2016-05-10 08:23:13 +0800

    fix the body content being cut-off

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,13 @@
-﻿import {Component} from 'angular2/core';
+﻿import {Component, OnInit} from 'angular2/core';
+declare var jQuery: any;
 
 @Component({
     selector: 'app',
     templateUrl: 'app/app.html'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+    ngOnInit() {
+        jQuery.AdminLTE.layout.activate();
+    }
 }
