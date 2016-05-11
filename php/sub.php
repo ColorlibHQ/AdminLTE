7067473d8954b4b55e1a1cb56c802f75758857c7
Mcat12 <newtoncat12@yahoo.com>
2016-05-11 16:31:07 -0400

    Return correct CORS header for Host
    If only Host was correct (FireFox and IE only set Host
    for same-origin requests) then it would still use the
    empty Origin header for the CORS response, leading to
    `Access-Control-Allow-Origin: `

--- php/sub.php
+++ php/sub.php
@@ -3,11 +3,12 @@ if(!isset($_POST['domain'], $_POST['list'], $_POST['token']))
     die("Missing POST variables");
 
 // Check CORS
-if($_SERVER['HTTP_ORIGIN'] != "http://pi.hole" && $_SERVER['HTTP_ORIGIN'] != "http://${_SERVER['SERVER_ADDR']}" &&
-    $_SERVER['HTTP_HOST'] != $_SERVER['SERVER_ADDR'] && $_SERVER['HTTP_HOST'] != "pi.hole")
+if($_SERVER['HTTP_ORIGIN'] == "http://pi.hole" || $_SERVER['HTTP_ORIGIN'] == "http://${_SERVER['SERVER_ADDR']}")
+    header("Access-Control-Allow-Origin: ${_SERVER['HTTP_ORIGIN']}");
+else if($_SERVER['HTTP_HOST'] == $_SERVER['SERVER_ADDR'] || $_SERVER['HTTP_HOST'] == "pi.hole")
+    header("Access-Control-Allow-Origin: ${_SERVER['HTTP_HOST']}");
+else
     die("Failed CORS");
-
-header("Access-Control-Allow-Origin: ${_SERVER['HTTP_ORIGIN']}");
 
 session_start();
 
