8a33af6d6be9073287188faf1056cd9f02531608
Mcat12 <newtoncat12@yahoo.com>
2016-05-10 20:30:28 -0400

    Possible fix for FireFox and IE
    Both failed CORS as both browsers, unlike Chrome,
    do not send `Origin` headers for same-origin requests.
    Now the scripts check if the `Host` header equals
    the IP of the Pi-hole. IE may require more fixing,
    as it's IE. :P

--- php/sub.php
+++ php/sub.php
@@ -3,7 +3,7 @@ if(!isset($_POST['domain'], $_POST['list'], $_POST['token']))
     die("Missing POST variables");
 
 // Check CORS
-if($_SERVER['HTTP_ORIGIN'] != "http://pi.hole" && $_SERVER['HTTP_ORIGIN'] != "http://${_SERVER['SERVER_ADDR']}")
+if($_SERVER['HTTP_ORIGIN'] != "http://pi.hole" && $_SERVER['HTTP_ORIGIN'] != "http://${_SERVER['SERVER_ADDR']}" || $_SERVER['HTTP_HOST'] != $_SERVER['SERVER_ADDR'])
     die("Failed CORS");
 
 header("Access-Control-Allow-Origin: ${_SERVER['HTTP_ORIGIN']}");
