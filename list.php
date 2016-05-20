3a6e6b119968e75c04235e4c34a0d53fe407e830
Mcat12 <newtoncat12@yahoo.com>
2016-05-19 20:17:56 -0400

    Convert commands to jQuery
    Mainly changed from `document.getElementById("something")` to
    `$(#something)`. Also changed the html and hide/show functions
    as a result of the change.

--- list.php
+++ list.php
@@ -61,49 +61,49 @@ require "footer.php";
             method: "get",
             data: {"list":"<?php echo $list ?>"},
             success: function(response) {
-                var list = document.getElementById("list");
-                list.innerHTML = "";
+                var list = $("#list");
+                list.html("");
                 var data = JSON.parse(response);
                 
                 if(data.length === 0) {
-                    list.innerHTML =
-                        '<div class="alert alert-info" role="alert">Your <?php getFullName(); ?> is empty!</div>';
+                    list.html('<div class="alert alert-info" role="alert">Your <?php getFullName(); ?> is empty!</div>');
                 }
                 else {
                     data.forEach(function (entry, index) {
-                        list.innerHTML +=
+                        list.append(
                             '<li id="' + index + '" class="list-group-item clearfix">' + entry +
                             '<button class="btn btn-danger btn-xs pull-right" type="button" onclick="sub(\'' + index + '\', \'' + entry + '\')">' +
-                            '<span class="glyphicon glyphicon-trash"></span></button></li>';
-                    })
+                            '<span class="glyphicon glyphicon-trash"></span></button></li>'
+                        );
+                    });
                 }
             },
             error: function(jqXHR, exception) {
-                document.getElementById("alFailure").hidden = false;
+                $("#alFailure").show();
             }
         });
     }
     
     function add() {
-        var domain = document.getElementById("domain").value;
-        if(domain.length === 0)
+        var domain = $("#domain");
+        if(domain.val().length === 0)
             return;
         
-        document.getElementById("alInfo").hidden = false;
-        document.getElementById("alSuccess").hidden = true;
-        document.getElementById("alFailure").hidden = true;
+        $("#alInfo").show();
+        $("#alSuccess").hide();
+        $("#alFailure").hide();
         $.ajax({
             url: "php/add.php",
             method: "post",
-            data: {"domain":domain, "list":"<?php echo $list ?>", "token":"<?php echo $token ?>"},
+            data: {"domain":domain.val(), "list":"<?php echo $list ?>", "token":"<?php echo $token ?>"},
             success: function(response) {
                 if(response.length !== 0)
                     return;
-                document.getElementById("alSuccess").hidden = false;
+                $("#alSuccess").show();
                 refresh();
             },
             error: function(jqXHR, exception) {
-                document.getElementById("alFailure").hidden = false;
+                $("#alFailure").show();
             }
         });
     }
@@ -117,7 +117,7 @@ require "footer.php";
             success: function(response) {
                 if(response.length !== 0)
                     return;
-                document.getElementById("list").removeChild(document.getElementById(index));
+                $("#list #"+index+"").remove();
             },
             error: function(jqXHR, exception) {
                 alert("Failed to remove the domain!");
