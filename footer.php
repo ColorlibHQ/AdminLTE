c050b8151ae9923b3c728ae910649e340bf92c14
Mcat12 <newtoncat12@yahoo.com>
2016-05-21 10:58:06 -0400

    Add update notifications
    Using the Github API, it checks the latest release against
    the current version. If it needs updating, it shows an
    update alert in the top right dropdown-menu and opens it
    automatically. Also this commit stops the dropdown-menu
    from closing if you click inside of it. It will only close
    if you click the toggle button or outside of it.

--- footer.php
+++ footer.php
@@ -4,8 +4,12 @@
     <!-- /.content-wrapper -->
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Pi-hole Version </b> <?php echo exec("cd /etc/.pihole/ && git describe --tags --abbrev=0"); ?>
-            <b>Web Interface Version </b> <?php echo exec("cd /var/www/html/admin/ && git describe --tags --abbrev=0"); ?>
+            <?php
+            $piholeVersion = exec("cd /etc/.pihole/ && git describe --tags --abbrev=0");
+            $webVersion = exec("cd /var/www/html/admin/ && git describe --tags --abbrev=0");
+            ?>
+            <b>Pi-hole Version </b> <?php echo $piholeVersion; ?>
+            <b>Web Interface Version </b> <?php echo $webVersion; ?>
         </div>
         <i class="fa fa-github"></i> <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY">Donate</a></strong> if you found this useful.
     </footer>
@@ -19,6 +23,58 @@
 <script src="js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="js/dataTables.bootstrap.min.js" type="text/javascript"></script>
 <script src="js/Chart.min.js"></script>
+
+<script>
+    // User menu toggle
+    $("#dropdown-menu a").on("click", function(event) {
+        $(this).parent().toggleClass("open");
+    });
+    $("body").on("click", function(event) {
+        if(!$("#dropdown-menu").is(event.target) && $("#dropdown-menu").has(event.target).length === 0) {
+            $("#dropdown-menu").removeClass("open");
+        }
+    });
+    
+    // Credit for following function: https://gist.github.com/alexey-bass/1115557
+    function versionCompare(left, right) {
+        if (typeof left + typeof right != 'stringstring')
+            return false;
+
+        var a = left.split('.')
+            ,   b = right.split('.')
+            ,   i = 0, len = Math.max(a.length, b.length);
+
+        for (; i < len; i++) {
+            if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
+                return 1;
+            } else if ((b[i] && !a[i] && parseInt(b[i]) > 0) || (parseInt(a[i]) < parseInt(b[i]))) {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+    
+    // Update check
+    $.getJSON("https://api.github.com/repos/pi-hole/pi-hole/releases/latest", function(json) {
+        if(versionCompare("<?php echo substr($piholeVersion, 1); ?>", json.tag_name.slice(1)) < 0) {
+            // Alert user
+            $("#alPiholeUpdate").show();
+            if(!$("#dropdown-menu").hasClass("open")) {
+                $("#dropdown-menu").addClass("open");
+            }
+        }
+    });
+    $.getJSON("https://api.github.com/repos/pi-hole/AdminLTE/releases/latest", function(json) {
+        if(versionCompare("<?php echo substr($webVersion, 1); ?>", json.tag_name.slice(1)) < 0) {
+            // Alert user
+            $("#alWebUpdate").show();
+            if(!$("#dropdown-menu").hasClass("open")) {
+                $("#dropdown-menu").addClass("open");
+            }
+        }
+    });
+</script>
 
 </body>
 </html>
