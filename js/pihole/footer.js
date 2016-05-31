3f66e9015ab73dadf5cb8f07f3aa3fe30d678fbe
Mcat12 <newtoncat12@yahoo.com>
2016-05-31 12:00:46 -0400

    Add prompt for users to update Pi-hole
    Since whitelisting from the web interface needs sudo, the
    Pi-hole core side needs to be updated to v2.6.4. That version
    installs the sudoers file needed to run pihole commands.
    Without that sudoers file installed, the web interface
    cannot make any changes.

--- js/pihole/footer.js
+++ js/pihole/footer.js
@@ -50,3 +50,10 @@ $.getJSON("https://api.github.com/repos/pi-hole/AdminLTE/releases/latest", funct
         }
     }
 });
+
+/*
+ * Make sure that Pi-hole is updated to at least v2.6.4, since that is needed to use the sudo
+ * features of the interface
+ */
+if(versionCompare(piholeVersion, "v2.6.4") < 0)
+    alert("Pi-hole needs to be updated to at least v2.6.4 before you can use features such as whitelisting/blacklisting from this web interface!")
