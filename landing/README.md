8b9c16e3306ba30368d8eaa14397677a53bdfa83
yuyangchee98 <hello@mytravelday.asia>
2016-06-04 23:18:03 +0800

    added landing page

---
+++ landing/README.md
@@ -0,0 +1,55 @@
+# "Cardio" One Page Website Template
+
+"Cardio" is a one page website template made by [PHIr](http://www.phir.co/) exclusively for Codrops.
+
+[Download on Codrops](http://tympanus.net/codrops/?p=24301)
+
+[Demo](http://tympanus.net/Freebies/Cardio/)
+
+## License
+
+Use it freely but please do not republish, distribute or sell "as-is". [Read more about our license.](http://tympanus.net/codrops/licensing/)
+
+## Credits 
+
+### Bootstrap
+
+[Bootstrap](http://getbootstrap.com/) by Twitter. Licensed under [MIT](https://github.com/twbs/bootstrap/blob/master/LICENSE)
+
+### Animate.css
+
+[Animate.css](https://daneden.github.io/animate.css/) by Dan Eden
+
+### Owl Carousel
+
+[Owl Carousel](http://owlgraphic.com/owlcarousel/) by Bartosz Wojciechowski. Licensed under [MIT](https://github.com/OwlFonk/OwlCarousel/blob/master/LICENSE)
+
+### WOW.js
+
+[WOW.js](http://mynameismatthieu.com/WOW/) by Matt Aussaguel. Licensed under [MIT](https://github.com/matthieua/WOW/blob/master/LICENSE-MIT)
+
+### jQuery One Page Nav
+
+[jQuery One Page Nav](http://davist11.github.io/jQuery-One-Page-Nav/) by Trevor Davis.
+
+### Font Awesome Icons 
+
+[Font Awesome](http://fortawesome.github.io/Font-Awesome/) by Dave Gandy. Licensed under [MIT](http://opensource.org/licenses/mit-license.html)
+
+### Elegant Icons 
+
+[Elegant Icons](http://www.elegantthemes.com/blog/resources/elegant-icon-font/) by Elegant Themes. Licensed under [MIT](http://opensource.org/licenses/mit-license.html)
+
+### Images
+
+[Image Pricing 1](https://www.flickr.com/photos/130855607@N05/16086503254/sizes/o/)
+
+Other images from [Pixabay](https://pixabay.com/)
+
+## Misc
+
+Follow us: [Twitter](http://www.twitter.com/codrops), [Facebook](http://www.facebook.com/pages/Codrops/159107397912), [Google+](https://plus.google.com/101095823814290637419), [GitHub](https://github.com/codrops), [Pinterest](http://www.pinterest.com/codrops/)
+
+[© Codrops 2015](http://www.codrops.com)
+
+
---
+++ landing/js/main.js
@@ -0,0 +1,171 @@
+jQuery(function($) {
+	"use strict";
+	// Author Code Here
+
+	var owlPricing;
+	var ratio = 2;
+
+	// Window Load
+	$(window).load(function() {
+		// Preloader
+		$('.intro-tables, .parallax, header').css('opacity', '0');
+		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			$('.preloader').hide();
+			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+			});
+		});
+
+		// Header Init
+		if ($(window).height() > $(window).width()) {
+			var ratio = $('.parallax').width() / $('.parallax').height();
+			$('.parallax img').css('height', ($(window).height()) + 'px');
+			$('.parallax img').css('width', $('.parallax').height() * ratio + 'px');
+		}
+
+		$('header').height($(window).height() + 80);
+		$('section .cut').each(function() {
+			if ($(this).hasClass('cut-top'))
+				$(this).css('border-right-width', $(this).parent().width() + "px");
+			else if ($(this).hasClass('cut-bottom'))
+				$(this).css('border-left-width', $(this).parent().width() + "px");
+		});
+
+		// Sliders Init
+		$('.owl-schedule').owlCarousel({
+			singleItem: true,
+			pagination: true
+		});
+		$('.owl-testimonials').owlCarousel({
+			singleItem: true,
+			pagination: true
+		});
+		$('.owl-twitter').owlCarousel({
+			singleItem: true,
+			pagination: true
+		});
+
+		// Navbar Init
+		$('nav').addClass('original').clone().insertAfter('nav').addClass('navbar-fixed-top').css('position', 'fixed').css('top', '0').css('margin-top', '0').removeClass('original');
+		$('.mobile-nav ul').html($('nav .navbar-nav').html());
+		$('nav.navbar-fixed-top .navbar-brand img').attr('src', $('nav.navbar-fixed-top .navbar-brand img').data("active-url"));
+
+		// Typing Intro Init
+		$(".typed").typewriter({
+			speed: 60
+		});
+
+		// Popup Form Init
+		var i = 0;
+		var interval = 0.15;
+		$('.popup-form .dropdown-menu li').each(function() {
+			$(this).css('animation-delay', i + "s");
+			i += interval;
+		});
+		$('.popup-form .dropdown-menu li a').click(function(event) {
+			event.preventDefault();
+			$(this).parent().parent().prev('button').html($(this).html());
+		});
+
+		// Onepage Nav
+		$('.navbar.navbar-fixed-top .navbar-nav').onePageNav({
+			currentClass: 'active',
+			changeHash: false,
+			scrollSpeed: 400,
+			filter: ':not(.btn)'
+		});
+	});
+	// Window Scroll
+	function onScroll() {
+		if ($(window).scrollTop() > 50) {
+			$('nav.original').css('opacity', '0');
+			$('nav.navbar-fixed-top').css('opacity', '1');
+		} else {
+			$('nav.original').css('opacity', '1');
+			$('nav.navbar-fixed-top').css('opacity', '0');
+		}
+	}
+
+	window.addEventListener('scroll', onScroll, false);
+
+	// Window Resize
+	$(window).resize(function() {
+		$('header').height($(window).height());
+	});
+
+	// Pricing Box Click Event
+	$('.pricing .box-main').click(function() {
+		$('.pricing .box-main').removeClass('active');
+		$('.pricing .box-second').removeClass('active');
+		$(this).addClass('active');
+		$(this).next($('.box-second')).addClass('active');
+		$('#pricing').css("background-image", "url(" + $(this).data('img') + ")");
+		$('#pricing').css("background-size", "cover");
+	});
+
+	// Mobile Nav
+	$('body').on('click', 'nav .navbar-toggle', function() {
+		event.stopPropagation();
+		$('.mobile-nav').addClass('active');
+	});
+
+	$('body').on('click', '.mobile-nav a', function(event) {
+		$('.mobile-nav').removeClass('active');
+		if(!this.hash) return;
+		event.preventDefault();
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			event.stopPropagation();
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+	$('body').on('click', '.mobile-nav a.close-link', function(event) {
+		$('.mobile-nav').removeClass('active');
+		event.preventDefault();
+	});
+
+	$('body').on('click', 'nav.original .navbar-nav a:not([data-toggle])', function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			event.stopPropagation();
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+	function centerModal() {
+		$(this).css('display', 'block');
+		var $dialog = $(this).find(".modal-dialog"),
+			offset = ($(window).height() - $dialog.height()) / 2,
+			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
+
+		// Make sure you don't hide the top part of the modal w/ a negative margin
+		// if it's longer than the screen height, and keep the margin equal to 
+		// the bottom margin of the modal
+		if (offset < bottomMargin) offset = bottomMargin;
+		$dialog.css("margin-top", offset);
+	}
+
+	$('.modal').on('show.bs.modal', centerModal);
+
+	$('.modal-popup .close-link').click(function(event){
+		event.preventDefault();
+		$('#modal1').modal('hide');
+	});
+
+	$(window).on("resize", function() {
+		$('.modal:visible').each(centerModal);
+	});
+});
