8e15275c225fac832f9ae93409736dbe02568c67
Arno Roldao Junior <arno@advg.com.br>
2016-06-23 10:27:27 -0300

    Separate: task 'BABEL' and create module for 'load-config-grunt'

---
+++ grunt-tasks/babel.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (grunt) {
+  return {
+    options: {
+      sourceMap: true,
+      presets: ['es2015']
+    },
+    dist: {
+      files: {
+        'dist/js/AdminLTE.js': 'build/js/AdminLTE.js'
+      }
+    }
+  };
+};
