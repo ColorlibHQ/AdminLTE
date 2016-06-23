5ba76d12701da60c20603ac9803d52959ddc7076
Arno Roldao Junior <arno@advg.com.br>
2016-06-23 10:25:16 -0300

    Separate: task 'SASS' and create module for 'load-config-grunt'

---
+++ grunt-tasks/sass.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function (grunt) {
+  return {
+      development: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/tmp/AdminLTE.css': 'build/scss/AdminLTE.scss'
+        }
+      },
+      production: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'dist/tmp/AdminLTE.min.css': 'build/scss/AdminLTE.scss'
+        }
+      }
+    };
+};
