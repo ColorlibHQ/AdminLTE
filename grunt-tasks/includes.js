acd331b6dc39efcd2a2dfe10b93a1ca699dc4d03
Arno Roldao Junior <arno@advg.com.br>
2016-06-23 10:28:15 -0300

    Separate: task 'INCLUDES' and create module for 'load-config-grunt'

---
+++ grunt-tasks/includes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function (grunt) {
+  return {
+    build: {
+      src: ['*.html'], // Source files
+      dest: 'documentation/', // Destination directory
+      flatten: true,
+      cwd: 'documentation/build',
+      options: {
+        silent: true,
+        includePath: 'documentation/build/include'
+      }
+    }
+  };
+};
