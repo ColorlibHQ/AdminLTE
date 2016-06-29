1d7f8fa444ca78dc75dc4e0bedc61c36a6ef5375
Arno Roldao Junior <arno@advg.com.br>
2016-06-28 21:12:20 -0300

    Separate: task 'CONCAT' and create module for 'load-config-grunt'

---
+++ grunt-tasks/concat.js
@@ -0,0 +1,20 @@
+// Concat compiled JS files
+'use strict';
+
+module.exports = function (grunt) {
+  return {
+    options: {
+      stripBanners: true,
+      banner: '<%= banner %>'
+    },
+    adminlte: {
+      src: [
+        'build/js/src/Layout.js',
+        'build/js/src/Treeview.js',
+        'build/js/src/PushMenu.js',
+        'build/js/src/Widget.js'
+      ],
+      dest: 'build/js/src/AdminLTE.js'
+    }
+  };
+};
