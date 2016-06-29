ba167fa22de256171c16f3d2d4af4987b762e7c7
Arno Roldao Junior <arno@advg.com.br>
2016-06-28 21:12:44 -0300

    Separate: task 'JSCS' and create module for 'load-config-grunt'

---
+++ grunt-tasks/jscs.js
@@ -0,0 +1,19 @@
+// Lint JS code
+'use strict';
+
+module.exports = function (grunt) {
+	return  {
+    options: {
+      config: 'build/js/.jscsrc'
+    },
+    grunt: {
+      src: ['Gruntfile.js']
+    },
+    core: {
+      src: 'js/src/*.js'
+    }
+    /*app: {
+     src: 'dist/js/app.js'
+     }*/
+  };
+};
