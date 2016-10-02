24180e2974804038563ec7b4a0e4c5c208d7a122
Ernest <ernest.rekel@gmail.com>
2016-10-02 11:38:52 +0300

    Update line chart to 2.x format

--- dist/js/pages/dashboard2.js
+++ dist/js/pages/dashboard2.js
@@ -13,76 +13,77 @@ $(function () {
 
   // Get context with jQuery - using jQuery's .get() method.
   var salesChartCanvas = $("#salesChart").get(0).getContext("2d");
-  // This will get the first returned node in the jQuery collection.
-  var salesChart = new Chart(salesChartCanvas);
 
   var salesChartData = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: "Electronics",
-        fillColor: "rgb(210, 214, 222)",
-        strokeColor: "rgb(210, 214, 222)",
-        pointColor: "rgb(210, 214, 222)",
-        pointStrokeColor: "#c1c7d1",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgb(220,220,220)",
-        data: [65, 59, 80, 81, 56, 55, 40]
+        label: "Digital Goods",
+        backgroundColor: "rgba(60,141,188,0.9)",
+        borderColor: "rgba(60,141,188,0.8)",
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 0,
+        pointHitRadius: 10,
+        data: [28, 48, 40, 19, 86, 27, 90]
       },
       {
-        label: "Digital Goods",
-        fillColor: "rgba(60,141,188,0.9)",
-        strokeColor: "rgba(60,141,188,0.8)",
-        pointColor: "#3b8bba",
-        pointStrokeColor: "rgba(60,141,188,1)",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(60,141,188,1)",
-        data: [28, 48, 40, 19, 86, 27, 90]
-      }
+        label: "Electronics",
+        backgroundColor: "rgba(210, 214, 222, 1)",
+        borderColor: "rgba(210, 214, 222, 1)",
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 0,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
     ]
   };
 
+    // http://www.chartjs.org/docs/#line-chart-chart-options
+    // These are the customisation options specific to Line charts.
+    // These options are merged with the global chart configuration options (http://www.chartjs.org/docs/#chart-configuration-global-configuration),
+    // and form the options of the chart
   var salesChartOptions = {
-    //Boolean - If we should show the scale at all
-    showScale: true,
-    //Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines: false,
-    //String - Colour of the grid lines
-    scaleGridLineColor: "rgba(0,0,0,.05)",
-    //Number - Width of the grid lines
-    scaleGridLineWidth: 1,
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
-    //Boolean - Whether the line is curved between points
-    bezierCurve: true,
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension: 0.3,
-    //Boolean - Whether to show a dot for each point
-    pointDot: false,
-    //Number - Radius of each point dot in pixels
-    pointDotRadius: 4,
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth: 1,
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius: 20,
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke: true,
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth: 2,
-    //Boolean - Whether to fill the dataset with a color
-    datasetFill: true,
-    //String - A legend template
-    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
-    //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio: true,
-    //Boolean - whether to make the chart responsive to window resizing
-    responsive: true
+      // Boolean - ( true ) - If false, the lines between points are not drawn
+      showLines: true,
+      // Boolean - ( false ) - If true, NaN data does not break the line
+      spanGaps: false,
+      // You can update the global chart configuration like this.
+      legend: {
+          display: false
+      },
+      tooltips: {
+          /*
+           Sets which elements appear in the tooltip. Acceptable options are 'single', 'label' or 'x-axis'.
+
+           'single' highlights the closest element.
+           'label' highlights elements in all datasets at the same X value.
+           'x-axis' also highlights elements in all datasets at the same X value, but activates when hovering anywhere within the vertical slice of the x-axis representing that X value.
+          */
+          mode: 'x-axis'
+      },
+      scales : {
+        xAxes : [ {
+            gridLines : {
+                display : false
+            }
+        } ],
+        yAxes : [ {
+            gridLines : {
+                display : false
+            }
+        } ]
+      },
+      maintainAspectRatio:false,
   };
 
   //Create the line chart
-  salesChart.Line(salesChartData, salesChartOptions);
+  new Chart(salesChartCanvas, {
+    type: 'line',
+    data: salesChartData,
+    options: salesChartOptions
+  });
 
   //---------------------------
   //- END MONTHLY SALES CHART -
@@ -93,7 +94,7 @@ $(function () {
   //-------------
   // Get context with jQuery - using jQuery's .get() method.
   var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-  var pieChart = new Chart(pieChartCanvas);
+  // var pieChart = new Chart(pieChartCanvas);
   var PieData = [
     {
       value: 700,
@@ -160,7 +161,7 @@ $(function () {
   };
   //Create pie or douhnut chart
   // You can switch between pie and douhnut using the method below.
-  pieChart.Doughnut(PieData, pieOptions);
+  // pieChart.Doughnut(PieData, pieOptions);
   //-----------------
   //- END PIE CHART -
   //-----------------
