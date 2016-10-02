ff50fa63a29116e6c2e518429f5fcc37630cf2a4
Ernest <ernest.rekel@gmail.com>
2016-10-02 11:44:43 +0300

    Update pie chart

--- dist/js/pages/dashboard2.js
+++ dist/js/pages/dashboard2.js
@@ -94,77 +94,59 @@ $(function () {
   //-------------
   // Get context with jQuery - using jQuery's .get() method.
   var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-  // var pieChart = new Chart(pieChartCanvas);
-  var PieData = [
-    {
-      value: 700,
-      color: "#f56954",
-      highlight: "#f56954",
-      label: "Chrome"
-    },
-    {
-      value: 500,
-      color: "#00a65a",
-      highlight: "#00a65a",
-      label: "IE"
-    },
-    {
-      value: 400,
-      color: "#f39c12",
-      highlight: "#f39c12",
-      label: "FireFox"
-    },
-    {
-      value: 600,
-      color: "#00c0ef",
-      highlight: "#00c0ef",
-      label: "Safari"
-    },
-    {
-      value: 300,
-      color: "#3c8dbc",
-      highlight: "#3c8dbc",
-      label: "Opera"
-    },
-    {
-      value: 100,
-      color: "#d2d6de",
-      highlight: "#d2d6de",
-      label: "Navigator"
-    }
-  ];
+  var pieData = {
+    labels: [
+        "Chrome",
+          "IE",
+          "FireFox",
+          "Safari",
+          "Opera",
+          "Navigator",
+      ],
+      datasets: [
+          {
+              data: [700, 500, 400, 600, 300, 100],
+              backgroundColor: [
+                  "#f56954",
+                  "#00a65a",
+                  "#f39c12",
+                  "#00c0ef",
+                  "#3c8dbc",
+                  "#d2d6de",
+              ],
+              hoverBackgroundColor: [
+                  "#f56954",
+                  "#00a65a",
+                  "#f39c12",
+                  "#00c0ef",
+                  "#3c8dbc",
+                  "#d2d6de",
+              ]
+          }
+      ]
+  }
+
+  // http://www.chartjs.org/docs/#doughnut-pie-chart-chart-options
   var pieOptions = {
-    //Boolean - Whether we should show a stroke on each segment
-    segmentShowStroke: true,
-    //String - The colour of each segment stroke
-    segmentStrokeColor: "#fff",
-    //Number - The width of each segment stroke
-    segmentStrokeWidth: 1,
-    //Number - The percentage of the chart that we cut out of the middle
-    percentageInnerCutout: 50, // This is 0 for Pie charts
-    //Number - Amount of animation steps
-    animationSteps: 100,
-    //String - Animation easing effect
-    animationEasing: "easeOutBounce",
-    //Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate: true,
-    //Boolean - Whether we animate scaling the Doughnut from the centre
-    animateScale: false,
-    //Boolean - whether to make the chart responsive to window resizing
-    responsive: true,
-    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio: false,
-    //String - A legend template
-    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-    //String - A tooltip template
-    tooltipTemplate: "<%=value %> <%=label%> users"
+      // Number - ( 50 - for doughnut, 0 - for pie ) - The percentage of the chart that is cut out of the middle.
+      // NOTE: don't set this value if you want to use a pie chart.
+      cutoutPercentage: 50,
+      // Number - ( -0.5 * Math.PI ) - Starting angle to draw arcs from.
+      rotation: -0.5 * Math.PI,
+      maintainAspectRatio: false,
+      // hide legend
+      legend: {
+          display: false
+      }
   };
+
   //Create pie or douhnut chart
-  // You can switch between pie and douhnut using the method below.
-  // pieChart.Doughnut(PieData, pieOptions);
-  //-----------------
-  //- END PIE CHART -
-  //-----------------
+  new Chart(pieChartCanvas, {
+      // type: 'pie',
+      type: 'doughnut',
+      data: pieData,
+      options: pieOptions
+  });
 
   /* jVector Maps
    * ------------
