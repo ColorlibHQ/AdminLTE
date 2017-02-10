955fe63b15674a1a096097865324aa2be11f20f5
Dusan Orlovic <duleorlovic@gmail.com>
2017-02-10 18:41:29 +0100

    Set content-wrapper height so we see last sidebar link #1182

--- dist/js/app.js
+++ dist/js/app.js
@@ -258,7 +258,7 @@ function _init() {
       $(".layout-boxed > .wrapper").css('overflow', 'hidden');
       //Get window height and the wrapper height
       var footer_height = $('.main-footer').outerHeight() || 0;
-      var neg = $('.main-header').outerHeight() + footer_height;
+      var header_height = $('.main-header').outerHeight();
       var window_height = $(window).height();
       var sidebar_height = $(".sidebar").height() || 0;
       //Set the min-height of the content and sidebar based on the
@@ -267,9 +267,9 @@ function _init() {
         $(".content-wrapper, .right-side").css('min-height', window_height - footer_height);
       } else {
         var postSetWidth;
-        if (window_height >= sidebar_height) {
-          $(".content-wrapper, .right-side").css('min-height', window_height - neg);
-          postSetWidth = window_height - neg;
+        if (window_height >= sidebar_height + header_height) {
+          $(".content-wrapper, .right-side").css('min-height', window_height - header_height - footer_height);
+          postSetWidth = window_height - header_height - footer_height;
         } else {
           $(".content-wrapper, .right-side").css('min-height', sidebar_height);
           postSetWidth = sidebar_height;
