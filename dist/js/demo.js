8183f2ad2087bcefeadf199c8ef0cffd7bb4f6d0
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-04-23 07:26:17 -0400

    Remove console logs from demo.js

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -74,7 +74,6 @@
     } else {
       $('.main-header').removeClass('border-bottom')
     }
-    console.log($(this).is(':checked'), 'hi')
   })
   $checkbox_container.append($navbar_border)
   $checkbox_container.append('<span>Navbar border</span>')
@@ -116,7 +115,6 @@
     })
 
     $sidebar.addClass(sidebar_class)
-    console.log('added ' + sidebar_class)
   }))
 
   $container.append('<h6>Light Sidebar Variants</h6>')
@@ -133,7 +131,6 @@
     })
 
     $sidebar.addClass(sidebar_class)
-    console.log('added ' + sidebar_class)
   }))
 
   var logo_skins = navbar_all_colors
