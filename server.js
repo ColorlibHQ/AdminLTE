948a1d21a672f21b139b3c8ef8b1630bd95e2c15
Lyubaikin <wolf1601@yandex.ru>
2018-06-19 16:23:04 +0400

    fix ejs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
     
 Object.assign=require('object-assign')
 
-app.engine('html', require('ejs').renderFile);
+
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
