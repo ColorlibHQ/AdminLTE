5527f621c9277cf0806e7cea719463b5f8e4069b
Alan Schio <alan@jmjsistemas.com.br>
2018-10-01 17:30:13 -0400

    resolve #1854

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -1078,7 +1078,7 @@ throw new Error('AdminLTE requires jQuery')
     //tree.find(Selector.open).removeClass(ClassName.open);
     parentLi.removeClass(ClassName.open);
     tree.slideUp(this.options.animationSpeed, function () {
-      //tree.find(Selector.open + ' > ' + Selector.treeview).slideUp();
+      tree.find(Selector.open + ' > ' + Selector.treeview).slideUp();
       $(this.element).trigger(collapsedEvent);
     }.bind(this));
   };
