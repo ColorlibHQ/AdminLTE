0f1f556d68da8658f3bc9729fe45f837a9a4a3b7
REJack <info@rejack.de>
2019-06-14 10:17:33 +0200

    removed ckeditor from package.json & removed jquery sparline lines in dashboard2.js

--- dist/js/pages/dashboard2.js
+++ dist/js/pages/dashboard2.js
@@ -487,47 +487,4 @@ $(function () {
   //   ]
   // })
 
-  /* SPARKLINE CHARTS
-   * ----------------
-   * Create a inline charts with spark line
-   */
-
-  //-----------------
-  //- SPARKLINE BAR -
-  //-----------------
-  $('.sparkbar').each(function () {
-    var $this = $(this)
-    $this.sparkline('html', {
-      type    : 'bar',
-      height  : $this.data('height') ? $this.data('height') : '30',
-      barColor: $this.data('color')
-    })
-  })
-
-  //-----------------
-  //- SPARKLINE PIE -
-  //-----------------
-  $('.sparkpie').each(function () {
-    var $this = $(this)
-    $this.sparkline('html', {
-      type       : 'pie',
-      height     : $this.data('height') ? $this.data('height') : '90',
-      sliceColors: $this.data('color')
-    })
-  })
-
-  //------------------
-  //- SPARKLINE LINE -
-  //------------------
-  $('.sparkline').each(function () {
-    var $this = $(this)
-    $this.sparkline('html', {
-      type     : 'line',
-      height   : $this.data('height') ? $this.data('height') : '90',
-      width    : '100%',
-      lineColor: $this.data('linecolor'),
-      fillColor: $this.data('fillcolor'),
-      spotColor: $this.data('spotcolor')
-    })
-  })
 })
