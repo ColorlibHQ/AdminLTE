29a3ba395305b2fac81d6d8766bb9e6e2ba4f7fd
Raphael Jackstadt <info@rejack.de>
2019-10-16 08:34:07 +0200

    fixed content height calc with less sidebar content (#2332)

--- build/js/Layout.js
+++ build/js/Layout.js
@@ -77,7 +77,11 @@ const Layout = (($) => {
 
       const max = this._max(heights)
 
-      $(Selector.CONTENT).css('min-height', max - heights.header)
+      if (max == heights.window) {
+        $(Selector.CONTENT).css('min-height', max - heights.header - heights.footer)
+      } else {
+        $(Selector.CONTENT).css('min-height', max - heights.header)
+      }
 
       if ($('body').hasClass(ClassName.LAYOUT_FIXED)) {
         $(Selector.CONTENT).css('min-height', max - heights.header - heights.footer)
