4754f57ee8da987de0485d508607f333f1e65c5b
REJack <info@rejack.de>
2019-11-04 09:55:25 +0100

    prep pre version

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -1,5 +1,5 @@
 /*!
- * AdminLTE v3.0.0 (https://adminlte.io)
+ * AdminLTE v3.0.1-pre (https://adminlte.io)
  * Copyright 2014-2019 Colorlib <http://colorlib.com>
  * Licensed under MIT (https://github.com/ColorlibHQ/AdminLTE/blob/master/LICENSE)
  */
