1af43bd9ead95c0e41e530dd8b840171ff8bb990
XhmikosR <xhmikosr@gmail.com>
2020-06-01 14:44:44 +0300

    Remove outdated plugin files

--- plugins/filterizr/layouts/getSameSizeLayoutPosition.d.ts
+++
@@ -1,8 +0,0 @@
-import { Position } from '../FilterItem';
-import FilterContainer from '../FilterContainer';
-/**
- * Same size layout for items that have the same width/height
- * @param filterContainer instance.
- */
-declare const getSameSizeLayoutPosition: (filterContainer: FilterContainer) => Position[];
-export default getSameSizeLayoutPosition;
