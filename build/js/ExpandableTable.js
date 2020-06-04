54673806a832e09c9e8d5391fcfaa211900c5534
REJack <info@rejack.de>
2020-06-05 00:21:55 +0200

    enhance expandable table

--- build/js/ExpandableTable.js
+++ build/js/ExpandableTable.js
@@ -5,121 +5,124 @@
  * --------------------------------------------
  */
 
-const ExpandableTable = (($) => {
-  /**
-   * Constants
-   * ====================================================
-   */
+import $ from 'jquery'
 
-  const NAME               = 'ExpandableTable'
-  const DATA_KEY           = 'lte.expandableTable'
-  const EVENT_KEY          = `.${DATA_KEY}`
-  const JQUERY_NO_CONFLICT = $.fn[NAME]
+/**
+  * Constants
+  * ====================================================
+  */
 
-  
-  const ClassName = {
-    TABLE  : '.expandable-table',
-    HEADER : '.expandable-header',
-    BODY   : 'expandable-body'
+const NAME = 'ExpandableTable'
+const DATA_KEY = 'lte.expandableTable'
+const EVENT_KEY = `.${DATA_KEY}`
+const JQUERY_NO_CONFLICT = $.fn[NAME]
+
+const Event = {
+  EXPANDED: `expanded${EVENT_KEY}`,
+  COLLAPSED: `collapsed${EVENT_KEY}`
+}
+
+const ClassName = {
+  HEADER: 'expandable-header',
+  BODY: 'expandable-body'
+}
+
+const Selector = {
+  HEADER: `.${ClassName.HEADER}`,
+  DATA_SELECTOR: 'data-expandable-table',
+  EXPANDED: 'expanded',
+  COLLAPSED: 'collapsed'
+}
+
+/**
+  * Class Definition
+  * ====================================================
+  */
+class ExpandableTable {
+  constructor(element, config) {
+    this._config = config
+    this._element = element
   }
-  
-  const Selector = {
-    TABLE: `${ClassName.TABLE}`,
-    DATA_SELECTOR: 'expandableTable',
-    EXPANDED: 'expanded',
-    COLLAPSE: 'collapsed',
-    TRIGGER: `${ClassName.HEADER}`
-  }
 
-  const Default = {
-    
-  }
+  // Public
 
-  /**
-   * Class Definition
-   * ====================================================
-   */
-  class ExpandableTable {
-    constructor(element, config) {
-      this._config = config
-      this._element = element
-    }
+  init() {
+    $(Selector.HEADER).each((_, $header) => {
+      // Next Child to the header will have the same column span as header
+      console.log($header)
+      $($header).next().children().first().attr('colSpan', $($header).children().length)
 
-    // Public
-
-    init(){
-      this._element.children().find(`${ClassName.HEADER}`).each(function (_, header){
-        // Next Child to the header will have the same column span as header 
-        $(header).next().children().first().attr('colSpan', $(header).children().length)
-        
-        // Setting up table design for the first time
-        const type = $(header).data(Selector.DATA_SELECTOR);
-        if(type === Selector.EXPANDED){
-          $(header).next().children().first().show()
-
-        }else if(type === Selector.COLLAPSE){
-          $(header).next().children().first().hide()
-        }
-      })
-    }
-
-    toggleRow(){
-      const type = this._element.data(Selector.DATA_SELECTOR);
-      const body = this._element.next().children().first()
-      if (type === Selector.EXPANDED) {
-        body.slideUp();
-        this._element.data(Selector.DATA_SELECTOR, Selector.COLLAPSE);
-      }else if (type === Selector.COLLAPSE){
-        body.slideDown();
-        this._element.data(Selector.DATA_SELECTOR, Selector.EXPANDED);
+      // Setting up table design for the first time
+      const $type = $($header).next().attr(Selector.DATA_SELECTOR)
+      const $body = $($header).next().children().first().children()
+      if ($type === Selector.EXPANDED) {
+        $body.show()
+      } else if ($type === Selector.COLLAPSED) {
+        $body.hide()
+        $body.parent().parent().addClass('d-none')
       }
-    }
+    })
+  }
 
-    // Static
-    
-    static _jQueryInterface(config) {
-      return this.each(function () {
-        let data = $(this).data(DATA_KEY)
-        const _options = $.extend({}, Default, $(this).data())
-
-        if (!data) {
-          data = new ExpandableTable($(this), _options)
-          $(this).data(DATA_KEY, data)
-        }
-
-        if (config === 'init' || config === 'toggleRow') {
-          data[config]()
-        }
+  toggleRow() {
+    const $element = this._element
+    const time = 500
+    const $type = $element.next().attr(Selector.DATA_SELECTOR)
+    const $body = $element.next().children().first().children()
+    $body.stop()
+    if ($type === Selector.EXPANDED) {
+      $body.slideUp(time, () => {
+        $element.next().addClass('d-none')
       })
+      $element.next().attr(Selector.DATA_SELECTOR, Selector.COLLAPSED)
+      $element.trigger($.Event(Event.COLLAPSED))
+    } else if ($type === Selector.COLLAPSED) {
+      $element.next().removeClass('d-none')
+      $body.slideDown(time)
+      $element.next().attr(Selector.DATA_SELECTOR, Selector.EXPANDED)
+      $element.trigger($.Event(Event.EXPANDED))
     }
   }
 
-  /**
-   * Data API
-   * ====================================================
-   */
-  $(ClassName.TABLE).ready(function () {
-    ExpandableTable._jQueryInterface.call($(this), 'init')
-  })
+  // Static
 
-  $(document).on('click', Selector.TRIGGER, function(event){
-    ExpandableTable._jQueryInterface.call($(this), 'toggleRow');
-  })
+  static _jQueryInterface(config) {
+    return this.each(function () {
+      let data = $(this).data(DATA_KEY)
+      if (!data) {
+        data = new ExpandableTable($(this))
+        $(this).data(DATA_KEY, data)
+      }
 
-  /**
-   * jQuery API
-   * ====================================================
-   */
-
-  $.fn[NAME] = ExpandableTable._jQueryInterface
-  $.fn[NAME].Constructor = ExpandableTable
-  $.fn[NAME].noConflict  = function () {
-    $.fn[NAME] = JQUERY_NO_CONFLICT
-    return ExpandableTable._jQueryInterface
+      if (config === 'init' || config === 'toggleRow') {
+        data[config]()
+      }
+    })
   }
+}
 
-  return ExpandableTable
-})(jQuery)
+/**
+  * Data API
+  * ====================================================
+  */
+$(ClassName.TABLE).ready(function () {
+  ExpandableTable._jQueryInterface.call($(this), 'init')
+})
 
+$(document).on('click', Selector.HEADER, function () {
+  ExpandableTable._jQueryInterface.call($(this), 'toggleRow')
+})
 
-export default ExpandableTable;
+/**
+  * jQuery API
+  * ====================================================
+  */
+
+$.fn[NAME] = ExpandableTable._jQueryInterface
+$.fn[NAME].Constructor = ExpandableTable
+$.fn[NAME].noConflict  = function () {
+  $.fn[NAME] = JQUERY_NO_CONFLICT
+  return ExpandableTable._jQueryInterface
+}
+
+export default ExpandableTable
