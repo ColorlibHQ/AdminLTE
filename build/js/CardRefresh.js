1ce6165330aafd1836c20875c60780b0d16d0497
Daniel <50356015+danny007in@users.noreply.github.com>
2020-12-30 13:04:17 +0530

    Fixed Errors of PR - Bump eslint-plugin-unicorn from 23.0.0 to 25.0.0 (#3267)
    Fixed Lint Errorshttps://github.com/ColorlibHQ/AdminLTE/pull/3265

--- build/js/CardRefresh.js
+++ build/js/CardRefresh.js
@@ -36,8 +36,7 @@ const Default = {
   loadOnInit: true,
   responseType: '',
   overlayTemplate: '<div class="overlay"><i class="fas fa-2x fa-sync-alt fa-spin"></i></div>',
-  onLoadStart() {
-  },
+  onLoadStart() {},
   onLoadDone(response) {
     return response
   }
