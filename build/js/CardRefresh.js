bd6d5742a6c28889e61fe6ccf86f3467b6f310ba
equada <81673782+equada@users.noreply.github.com>
2021-03-31 15:41:49 +0200

    fix lint again

--- build/js/CardRefresh.js
+++ build/js/CardRefresh.js
@@ -77,9 +77,9 @@ class CardRefresh {
       this._removeOverlay()
     }, this._settings.responseType !== '' && this._settings.responseType).fail((jqXHR, textStatus, errorThrown) => {
       this._removeOverlay()
-      let msg = $(this.__settings.errorTemplate).text(errorThrown)
+      const msg = $(this.__settings.errorTemplate).text(errorThrown)
       this._parent.find(this._settings.content).empty().append(msg)
-      
+
       this._settings.onLoadFail.call($(this), jqXHR, textStatus, errorThrown)
     })
 
