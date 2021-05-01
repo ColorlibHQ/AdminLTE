453cfaf07b4d605c9ae50922b3a361cec1ad636e
Daniel <50356015+danny007in@users.noreply.github.com>
2021-05-01 22:17:38 +0530

    Update getTarget.js

--- build/config/getTarget.js
+++ build/config/getTarget.js
@@ -14,19 +14,18 @@ function getBuildTargets(targ) {
       'node',
       'safari'
     ]
+
+  const getEveryTar = browserslist().reverse()
   const sep = ' '
-  const supported = b => SUPPORTED_BUILD_TARGETS.some(t => b.startsWith(t + sep)) ? b.replace(sep, '') : undefined
-
-  const getEveryTar = browserslist().map(element => supported(element)).filter(Boolean).reverse()
-
-  let singleTar = ''
   const targets = []
+  let singleTar = ''
   let i = 0
+
   for (const tar of getEveryTar) {
     for (const selTar of SUPPORTED_BUILD_TARGETS) {
-      if (tar.startsWith(selTar) && !singleTar.startsWith(selTar)) {
-        singleTar = tar
+      if (tar.startsWith(selTar + sep) && !singleTar.startsWith(selTar)) {
         i++
+        singleTar = tar.replace(sep, '')
         targets[i] = singleTar
       }
     }
