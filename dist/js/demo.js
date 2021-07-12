e6139c15deae7a82f71870dabb5ca19c6e9f3f4e
REJack <info@rejack.de>
2021-07-12 22:35:59 +0200

    add demo.js reminder if not loaded via browserSync

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -10,6 +10,13 @@
 (function ($) {
   'use strict'
 
+  setTimeout(() => {
+    if (window.___browserSync___ === undefined) {
+      // eslint-disable-next-line no-alert
+      alert('You load AdminLTE\'s "demo.js", \nthis file is only created for testing purposes!')
+    }
+  }, 1000)
+
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
