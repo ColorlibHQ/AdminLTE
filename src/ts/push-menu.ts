d7054948645b9e9e580d0eefe3fd1cd6df6fe3cc
Gints Murāns <gm@gm.lv>
2021-12-13 10:24:43 +0200

    Added remember state option to pushmenu/sidebar menu

--- src/ts/push-menu.ts
+++ src/ts/push-menu.ts
@@ -14,6 +14,17 @@ import {
  * Constants
  * ------------------------------------------------------------------------
  */
+const DATA_KEY = 'lte.pushmenu'
+const EVENT_KEY = `.${DATA_KEY}`
+
+const EVENT_EXPAND = `expand${EVENT_KEY}`
+const EVENT_COLLAPSE = `collapse${EVENT_KEY}`
+const EVENT_CLOSE = `close${EVENT_KEY}`
+
+const DATA_NAME_REMEMBER_STATE = `${DATA_KEY}.remember.state`
+
+const COOKIE_REMEMBER_STATE = `${DATA_KEY}.sidebar.state`
+const COOKIE_PATH = `${DATA_KEY}.sidebar.cookie.path`
 
 const CLASS_NAME_SIDEBAR_MINI = 'sidebar-mini'
 const CLASS_NAME_SIDEBAR_MINI_HAD = 'sidebar-mini-had'
@@ -40,6 +51,12 @@ const Defaults = {
   onLayouMobile: 992
 }
 
+enum RememberState {
+  Open = 'Open',
+  Closed = 'Closed',
+  Collapsed = 'Collapsed'
+}
+
 /**
  * Class Definition
  * ====================================================
@@ -49,6 +66,9 @@ class PushMenu {
   _element: HTMLElement | undefined
   _config: undefined
   _bodyClass: DOMTokenList
+  _rememberState: boolean
+  _cookiePath: string
+
   constructor(element: HTMLElement | undefined, config: undefined) {
     this._element = element
 
@@ -56,6 +76,20 @@ class PushMenu {
     this._bodyClass = bodyElement.classList
 
     this._config = config
+
+    this._rememberState = false
+    this._cookiePath = '/'
+
+    if (this._element !== null) {
+      const remember: string = this._element ? this._element.dataset[DATA_NAME_REMEMBER_STATE] ?? '0' : '0'
+      const rememberInt = Number.parseInt(remember, 10)
+      this._rememberState = (rememberInt === 1)
+      if (this._rememberState && this._element && !this._element.id) {
+        throw new Error('To remember menu state, id parameter on menu button must be defined!')
+      }
+
+      this._cookiePath = this._element ? this._element.dataset[COOKIE_PATH] ?? this._cookiePath : this._cookiePath
+    }
   }
 
   sidebarOpening(): void {
@@ -96,12 +130,26 @@ class PushMenu {
     this._bodyClass.remove(CLASS_NAME_SIDEBAR_CLOSE)
     this._bodyClass.remove(CLASS_NAME_SIDEBAR_COLLAPSE)
     this._bodyClass.add(CLASS_NAME_SIDEBAR_OPEN)
+
+    if (this._element !== null) {
+      const event = new CustomEvent(EVENT_EXPAND)
+      this._element.dispatchEvent(event)
+    }
+
+    this.setState(RememberState.Open)
   }
 
   collapse(): void {
     this.sidebarColllapsing()
 
     this._bodyClass.add(CLASS_NAME_SIDEBAR_COLLAPSE)
+
+    if (this._element !== null) {
+      const event = new CustomEvent(EVENT_COLLAPSE)
+      this._element.dispatchEvent(event)
+    }
+
+    this.setState(RememberState.Collapsed)
   }
 
   close(): void {
@@ -112,6 +160,13 @@ class PushMenu {
     if (this._bodyClass.contains(CLASS_NAME_SIDEBAR_HORIZONTAL)) {
       this.menusClose()
     }
+
+    if (this._element !== null) {
+      const event = new CustomEvent(EVENT_CLOSE)
+      this._element.dispatchEvent(event)
+    }
+
+    this.setState(RememberState.Closed)
   }
 
   sidebarHover(): void {
@@ -128,7 +183,44 @@ class PushMenu {
     }
   }
 
-  addSidebaBreakPoint(): void {
+  setState(state: RememberState): void {
+    if (this._rememberState) {
+      window.document.cookie = `${COOKIE_REMEMBER_STATE}.${this._element!.id}=${state}; SameSite=Strict; Path=${this._cookiePath}`
+    }
+  }
+
+  initPreviousState(): void {
+    if (!this._rememberState) {
+      return
+    }
+
+    this._bodyClass.add('hold-transition')
+
+    const allcookies = document.cookie
+    const cookiearray = allcookies.split(';')
+
+    let state: RememberState = RememberState.Open
+    for (const item of cookiearray) {
+      const itemSplit = item.split('=')
+      if (itemSplit.length > 1 && itemSplit[0].trim() === `${COOKIE_REMEMBER_STATE}.${this._element!.id}`) {
+        state = RememberState[itemSplit[1].trim() as keyof typeof RememberState]
+      }
+    }
+
+    if (state === RememberState.Closed) {
+      this.close()
+    } else if (state === RememberState.Collapsed) {
+      this.collapse()
+    } else {
+      this.expand()
+    }
+
+    setTimeout(() => {
+      this._bodyClass.remove('hold-transition')
+    }, 100)
+  }
+
+  addSidebarBreakPoint(): void {
     const bodyClass = document.body.classList
     const widthOutput = window.innerWidth
 
@@ -138,7 +230,9 @@ class PushMenu {
 
     if (widthOutput >= Defaults.onLayouMobile) {
       bodyClass.remove(CLASS_NAME_LAYOUT_MOBILE)
-      this.expand()
+      if (!this._rememberState) {
+        this.initPreviousState()
+      }
     }
   }
 
@@ -185,7 +279,7 @@ class PushMenu {
   }
 
   init() {
-    this.addSidebaBreakPoint()
+    this.addSidebarBreakPoint()
     this.sidebarHover()
 
     const selSidebarSm = document.querySelector(SELECTOR_SIDEBAR_SM)
@@ -217,6 +311,9 @@ domReady(() => {
   const fullBtn = document.querySelectorAll(SELECTOR_FULL_TOGGLE)
 
   for (const btn of fullBtn) {
+    const data = new PushMenu(btn as HTMLElement, undefined)
+    data.initPreviousState()
+
     btn.addEventListener('click', event => {
       event.preventDefault()
 
@@ -236,6 +333,9 @@ domReady(() => {
   const miniBtn = document.querySelectorAll(SELECTOR_MINI_TOGGLE)
 
   for (const btn of miniBtn) {
+    const data = new PushMenu(btn as HTMLElement, undefined)
+    data.initPreviousState()
+
     btn.addEventListener('click', event => {
       event.preventDefault()
 
