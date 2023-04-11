2ddba3d72f6d647e9aaf3ab8dd4d822e569b90a0
Daniel <50356015+danny007in@users.noreply.github.com>
2023-04-11 16:50:10 +0530

    path with proper links

---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+function convertPathToHtml(path) {
+  let count = 0
+  while (path.startsWith('../')) {
+    count++
+    path = path.slice(3)
+  }
+
+  if (count === 2) {
+    return '.'
+  }
+
+  if (count === 3) {
+    return '..'
+  }
+
+  if (count === 4) {
+    return '../..'
+  }
+
+  if (count === 5) {
+    return '../../..'
+  }
+
+  return ''
+}
+
+export { convertPathToHtml }
