b396e718aa99546a05499d11cbd56287dfccbfea
Cláudio Patrício <cpatricio@ua.pt>
2023-05-03 04:38:29 +0100

    Update index.js
    Improved path conversion to allow any number of subpaths

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,12 @@
 function convertPathToHtml(path) {
-  let count = 0
+  let htmlpath = ''
   while (path.startsWith('../')) {
-    count++
     path = path.slice(3)
+    if (htmlpath.length < 2) htmlpath += '.'
+    else htmlpath += '/..'
   }
 
-  if (count === 1) {
-    return '.'
-  }
-
-  if (count === 2) {
-    return '..'
-  }
-
-  if (count === 3) {
-    return '../..'
-  }
-
-  if (count === 4) {
-    return '../../..'
-  }
-
-  return ''
+  return htmlpath
 }
 
 export { convertPathToHtml }
