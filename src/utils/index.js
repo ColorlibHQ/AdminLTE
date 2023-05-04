3e1133679b4f5cb956f58ea712a6abb392ddb8d9
Cláudio Patrício <cpatricio@ua.pt>
2023-05-04 13:51:03 +0100

    Update index.js
    lint fixed

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,13 @@ function convertPathToHtml(path) {
   let htmlpath = ''
   while (path.startsWith('../')) {
     path = path.slice(3)
-    if (htmlpath.length < 2) htmlpath += '.'
-    else htmlpath += '/..'
+    if (htmlpath.length < 2) {
+      htmlpath += '.'
+    }
+    else {
+      htmlpath += '/..'
+    }
   }
-
   return htmlpath
 }
 
