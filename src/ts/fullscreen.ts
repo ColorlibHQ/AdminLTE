310f7f01884a60338619eb4d023a8ec115670ff1
Walter Burditt <burdittw@gmail.com>
2023-09-01 00:06:56 -0700

    Update fullscreen.ts

--- src/ts/fullscreen.ts
+++ src/ts/fullscreen.ts
@@ -5,8 +5,8 @@
  * --------------------------------------------
  */
 
- import {
-    domReady
+import {
+  onDOMContentLoaded
 } from './util/index'
 
 /**
@@ -16,25 +16,24 @@
 
 const CLASS_NAME_FULLSCREEN = 'fullscreen'
 const CLASS_NAME_FULLSCREEN_ICON = 'fullscreen-icon'
-const CLASS_NAME_LTE_SHOW = "lte-show"
+const CLASS_NAME_LTE_SHOW = 'lte-show'
 
 /**
  * Class Definition
  * ====================================================
  */
 class FullScreen {
-    toggleFullScreen(): void {
-        if (document.fullscreenEnabled) {
-            if (!document.fullscreenElement) {
-                document.documentElement.requestFullscreen();
-
-            } else {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                }
-            }
+  toggleFullScreen(): void {
+    if (document.fullscreenEnabled) {
+      if (document.fullscreenElement) {
+        if (document.exitFullscreen) {
+          void document.exitFullscreen()
         }
+      } else {
+        void document.documentElement.requestFullscreen()
+      }
     }
+  }
 }
 
 /**
@@ -43,38 +42,38 @@ class FullScreen {
  * ====================================================
  */
 
-domReady(() => {
-    const button = document.getElementsByClassName(CLASS_NAME_FULLSCREEN)
+onDOMContentLoaded(() => {
+  const button = Array.from(document.getElementsByClassName(CLASS_NAME_FULLSCREEN))
 
-    for (const btn of button) {
-        btn.addEventListener('click', event => {
-            event.preventDefault()
-            const data = new FullScreen()
-            data.toggleFullScreen()
-        })
+  for (const btn of button) {
+    btn.addEventListener('click', event => {
+      event.preventDefault()
+      const data = new FullScreen()
+      data.toggleFullScreen()
+    })
+  }
+
+  /**
+  * If F12 DevTools are open and in the browser window then the
+  * height/width will be off and this does not work as expected
+  */
+  window.addEventListener('resize', () => {
+    const windowWidth = window.innerWidth * window.devicePixelRatio
+    const windowHeight = window.innerHeight * window.devicePixelRatio
+    const screenWidth = window.screen.width
+    const screenHeight = window.screen.height
+    if (((windowWidth / screenWidth) >= 0.95) && ((windowHeight / screenHeight) >= 0.95)) {
+      const icon = Array.from(document.getElementsByClassName(CLASS_NAME_FULLSCREEN_ICON))
+      for (const i of icon) {
+        i.classList.toggle(CLASS_NAME_LTE_SHOW)
+      }
+    } else {
+      const icon = Array.from(document.getElementsByClassName(CLASS_NAME_FULLSCREEN_ICON))
+      for (const i of icon) {
+        i.classList.toggle(CLASS_NAME_LTE_SHOW)
+      }
     }
-
-    /**
-    * If F12 DevTools are open and in the browser window then the 
-    * height/width will be off and this does not work as expected
-     */
-    window.addEventListener('resize', () => {
-        const windowWidth = window.innerWidth * window.devicePixelRatio;
-        const windowHeight = window.innerHeight * window.devicePixelRatio;
-        const screenWidth = window.screen.width;
-        const screenHeight = window.screen.height;
-        if (((windowWidth / screenWidth) >= 0.95) && ((windowHeight / screenHeight) >= 0.95)) {
-            const icon = document.getElementsByClassName(CLASS_NAME_FULLSCREEN_ICON)
-            for (const i of icon) {
-                i.classList.toggle(CLASS_NAME_LTE_SHOW);
-            }
-        } else {
-            const icon = document.getElementsByClassName(CLASS_NAME_FULLSCREEN_ICON)
-            for (const i of icon) {
-                i.classList.toggle(CLASS_NAME_LTE_SHOW);
-            }
-        }
-    });
+  })
 })
 
 export default FullScreen
